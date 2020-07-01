267268a59a9d9fd95c158c268998b450a9580bd2
Lizarr15 <lizarragafranco75@gmail.com>
2020-07-01 16:19:57 -0300

    ultimo intento o renucio XD

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-let fs = require ("fs")  ;  //filesistem lee y escribe archivos
-//console.log(fs)
-let ListaDeTareas = fs.readFileSync('./tareas.json', 'utf-8')
-let tareas = JSON.parse(ListaDeTareas)
-//console.log(tareas)
-//let texto = fs.readFileSync('./texto.txt','utf-8') //leo contenido de archivo
-//console.log(texto)
-let process = require ("process");
-let  comando = process.argv [2] //argv toma dato de la consola y lo ejecuta en el codigo, devuelve un array de posiciones
+// let fs = require ("fs")  ;  //filesistem lee y escribe archivos
+// //console.log(fs)
+// let ListaDeTareas = fs.readFileSync('./tareas.json', 'utf-8')
+// let tareas = JSON.parse(ListaDeTareas)
+// //console.log(tareas)
+// //let texto = fs.readFileSync('./texto.txt','utf-8') //leo contenido de archivo
+// //console.log(texto)
+// let process = require ("process");
+// let  comando = process.argv [2] //argv toma dato de la consola y lo ejecuta en el codigo, devuelve un array de posiciones
 
-switch(comando){
-    case 'Listamos':
-       for  (let i=0; i<tareas.length;i++){
-       console.log('Titulo:' + tareas[i].titulo)
-    }
-    //console.log('mostrar lista de tareas')
-        break
-    case 'Agregamos':
-        let nuevoTitulo = process.argv[3]
-        let nuevaTarea = {
-        titulo: nuevoTitulo,
-        estado: "pendiente"
-    }
-     tareas.push(nuevaTarea);
-    let nuevaListaDeTareas = JSON.stringify(tareas)
-     fs.writeFileSync('./tareas.json',nuevaListaDeTareas,'utf-8')
-        break
-         case undefined:
-        //console.log('debes escribir un comando')
-        break
+const { DefaultDeserializer } = require("v8");
+
+// switch(comando){
+//     case 'Listamos':
+//        for  (let i=0; i<tareas.length;i++){
+//        console.log('Titulo:' + tareas[i].titulo)
+//     }
+//     //console.log('mostrar lista de tareas')
+//         break
+//     case 'Agregamos':
+//         let nuevoTitulo = process.argv[3]
+//         let nuevaTarea = {
+//         titulo: nuevoTitulo,
+//         estado: "pendiente"
+//     }
+//      tareas.push(nuevaTarea);
+//     let nuevaListaDeTareas = JSON.stringify(tareas)
+//      fs.writeFileSync('./tareas.json',nuevaListaDeTareas,'utf-8')
+//         break
+//          case undefined:
+//         //console.log('debes escribir un comando')
+//         break
+// }
+function apellido(nan){
+    return "Bienvenido, " + nan;
 }
+
+console.log(apellido("lopez"))
+sadasdas
+asdfasda
+DefaultDeserializerasd
+asdfasdaasdas
+das
